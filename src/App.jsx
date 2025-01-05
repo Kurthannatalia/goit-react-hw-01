c690050061674cf91d314f42eef177a691d93650
natalia_kurthan <kurthannatalia@gmail.com>
2025-01-05 22:42:39 +0300

    upd

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,24 @@
-export default function App() {
+import Profile from "./components/Profile/Profile";
+import userData from "./userData.json";
+import "modern-normalize";
+import friends from "./Friends.json";
+import FriendList from "./components/FriendList/FriendList";
+import transactions from "./transactions.json";
+import TransactionHistory from "./components/TransactionHistory/TransactionHistory";
+
+export const App = () => {
   return (
-    <div>
-      <h1>Hello, Vite + React!</h1>
-    </div>
+    <>
+      <Profile
+        name={userData.username}
+        tag={userData.tag}
+        location={userData.location}
+        image={userData.avatar}
+        stats={userData.stats}
+      />
+      <FriendList friends={friends} />
+      <TransactionHistory transactions={transactions} />
+    </>
   );
-}
+};
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css'; // Імпорт стилів (якщо є)
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
 
-import App from './App'; // Імпорт головного компонента
-
-ReactDOM.createRoot(document.getElementById('app')).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
-);
+  </React.StrictMode>,
+)
